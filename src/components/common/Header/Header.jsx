0efcc3bedac6c86700228c794ac3891aea16af3a
Sylvain-MED <146923279+Sylvain-MED@users.noreply.github.com>
2024-06-05 00:31:33 +0200

    Logout button ok

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { FaCircleUser } from "react-icons/fa6";
+import { FaSignOutAlt } from "react-icons/fa";
 import logo from "../../../assets/argentBankLogo.png";
 import style from "./Header.module.scss";
 import Toast from "../Toast/Toast";
+import { updateToken } from "../../../store/userReducer";
 
 function Header() {
   const toast = useSelector((state) => state.toast);
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    dispatch(updateToken(null));
+  };
 
   return (
     <header id={style.header}>
@@ -17,11 +26,27 @@ function Header() {
       </div>
       <nav id={style.nav}>
         <ul>
-          <li>
-            <Link to="/signin">
-              <FaCircleUser /> Sign In
-            </Link>
-          </li>
+          {!user.token && (
+            <li>
+              <Link to="/signin">
+                <FaCircleUser /> Sign In
+              </Link>
+            </li>
+          )}
+          {user.token && (
+            <div className={style.userNav}>
+              <li>
+                <Link to="/user">
+                  <FaCircleUser /> Tony
+                </Link>
+              </li>
+              <li>
+                <button type="button" id={style.logOutButton} onClick={logout}>
+                  <FaSignOutAlt /> Sign out
+                </button>
+              </li>
+            </div>
+          )}
         </ul>
       </nav>
       {toast.show === true && <Toast />}
