8c9ef9c57321b9ce00347e820ec35b790715ee97
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-06-28 13:37:52 +0200

    Responsive adjustements

--- src/routes/User/Account/Account.jsx
+++ src/routes/User/Account/Account.jsx
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 function Account({ title, amount, description }) {
   return (
     <section className={style.account}>
-      <div className="account-content-wrapper">
+      <div className={style.accountContentWrapper}>
         <h3 className={style.accountTitle}>{title}</h3>
         <p className={style.accountAmount}>${amount}</p>
         <p className="account-amount-description">{description}</p>
       </div>
-      <button className="transaction-button">View transactions</button>
+      <button className={style.transactionButton}>View transactions</button>
     </section>
   );
 }
